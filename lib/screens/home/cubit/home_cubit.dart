3668842c396bbc56ed3335158f919538701de937
mediocre9 <mirzafahadzia9@gmail.com>
2023-01-17 23:45:59 +0500

    refactoring and fix device pairing bug

--- lib/screens/home/cubit/home_cubit.dart
+++ lib/screens/home/cubit/home_cubit.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
-
 import 'package:bloc/bloc.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 import 'package:remo_tooth/config/app_strings.dart';
 
@@ -9,52 +6,19 @@ part 'home_state.dart';
 
 class HomeCubit extends Cubit<HomeState> {
   final FlutterBluetoothSerial _bluetooth = FlutterBluetoothSerial.instance;
-  final List<BluetoothDevice> _foundedDevices = [];
-  BluetoothDevice? _connectedDevice;
-  HomeCubit() : super(Initial()) {
-    getPairedDevices();
+  final List<BluetoothDevice> _foundedDevices = List.empty(growable: true);
+
+  HomeCubit() : super(Initial(message: 'Scan for beacon devices')) {
+    _getPairedDevices();
   }
 
   Future<bool?> get isBluetoothEnabled async => _bluetooth.isEnabled;
 
-  Future establishConnectionToDevice(BluetoothDevice device) async {
-    if (!device.isBonded) {
-      emit(Loading(message: 'Connecting to ${device.name} . . .'));
-      await _bluetooth.bondDeviceAtAddress(device.address).then((connState) {
-        if (connState!) {
-          _connectedDevice = device;
-          emit(BluetoothResponse(message: 'Connected to ${device.name}!'));
-          emit(Connected());
-        } else {
-          emit(BluetoothResponse(
-              message: 'Couldn`t connect to ${device.name}!'));
-          emit(ShowDevices(
-            totalDevices: _foundedDevices.length,
-            devices: _foundedDevices,
-          ));
-        }
-      });
-    }
-  }
-
-  Future unPairDevice(BluetoothDevice device) async {
-    _foundedDevices.removeWhere((element) => element.address == device.address);
-    if (device.isBonded) {
-      await _bluetooth
-          .removeDeviceBondWithAddress(device.address)
-          .then((isUnPairSucessful) {
-        if (isUnPairSucessful!) {
-          emit(BluetoothResponse(
-              message: 'Device ${device.name} has been unpaired!'));
-        }
-      });
-    }
-  }
-
-  Future getPairedDevices() async {
+  /// retrieves all paired devices.
+  Future<void> _getPairedDevices() async {
+    _foundedDevices.clear();
     if ((await isBluetoothEnabled)!) {
-      _foundedDevices.clear();
-      emit(Loading(message: 'Getting paired devices . . .'));
+      emit(Discovering(message: 'Getting paired devices . . .'));
       await _bluetooth
           .getBondedDevices()
           .then((paired) => _foundedDevices.addAll(paired))
@@ -69,15 +33,22 @@ class HomeCubit extends Cubit<HomeState> {
         }
       });
     } else {
-      await _bluetooth.requestEnable();
-      emit(BluetoothResponse(message: AppString.BLUE_TOOTH_SERVICE_MSG));
+      await _bluetooth.requestEnable().then((isBluetoothTurnedOn) {
+        if (isBluetoothTurnedOn!) {
+          _getPairedDevices();
+        } else {
+          // emit(BluetoothResponse(message: AppString.DISABLED_BLUETOOTH_MSG));
+          emit(Initial(message: 'Scan for beacon devices'));
+        }
+      });
     }
   }
 
-  Future discoverDevices() async {
+  /// finds nearby beacon bluetooth devices for pairing.
+  Future<void> discoverDevices() async {
+    _foundedDevices.clear();
     if ((await isBluetoothEnabled)!) {
-      _foundedDevices.clear();
-      emit(Loading(message: AppString.ON_SCAN_MSG));
+      emit(Discovering(message: AppString.DISCOVERING_MSG));
       _bluetooth.startDiscovery().listen((e) {
         _foundedDevices.add(e.device);
       }).onDone(() {
@@ -87,28 +58,58 @@ class HomeCubit extends Cubit<HomeState> {
             devices: _foundedDevices,
           ));
         } else {
-          emit(BluetoothResponse(message: AppString.DEVICES_NOT_FOUND));
+          emit(BluetoothResponse(message: AppString.UNDISCOVERED_DEVICES_MSG));
+          emit(Initial(message: 'Scan for beacon devices'));
         }
       });
     } else {
-      await _bluetooth.requestEnable();
-      emit(BluetoothResponse(message: AppString.BLUE_TOOTH_SERVICE_MSG));
+      await _bluetooth.requestEnable().then((isBluetoothTurnedOn) {
+        if (isBluetoothTurnedOn!) {
+          _getPairedDevices();
+        } else {
+          emit(BluetoothResponse(message: AppString.DISABLED_BLUETOOTH_MSG));
+          emit(Initial(message: 'Scan for beacon devices'));
+        }
+      });
     }
   }
 
-  Future onMessage() async {
-    emit(Loading(message: 'Sending 1-bit status signal. . .'));
-    BluetoothConnection con =
-        await BluetoothConnection.toAddress(_connectedDevice!.address);
-    con.output.add(Uint8List.fromList(utf8.encode('1')));
-    emit(BluetoothResponse(message: '1-bit status signal!'));
+  /// Tries to pair with discovered unpaired device.
+  Future<void> pairDevice(BluetoothDevice device) async {
+    if (!device.isBonded) {
+      emit(Pairing(
+          message: 'Trying to pair with ${device.name}. Please be patient!'));
+      await _bluetooth.bondDeviceAtAddress(device.address).then((connState) {
+        if (connState!) {
+          device;
+          emit(BluetoothResponse(message: 'Paired to ${device.name}!'));
+          emit(Paired(device: device));
+        } else {
+          emit(
+              BluetoothResponse(message: 'Couldn`t pair with ${device.name}!'));
+          emit(ShowDevices(
+            totalDevices: _foundedDevices.length,
+            devices: _foundedDevices,
+          ));
+        }
+      });
+    } else if (device.isBonded) {
+      emit(Paired(device: device));
+    }
   }
 
-  Future offMessage() async {
-    emit(Loading(message: 'Sending 1-bit status signal. . .'));
-    BluetoothConnection con =
-        await BluetoothConnection.toAddress(_connectedDevice!.address);
-    con.output.add(Uint8List.fromList(utf8.encode('0')));
-    emit(BluetoothResponse(message: '0-bit status signal!'));
+  /// Unpairs a bonded/paired device.
+  Future<void> unPairDevice(BluetoothDevice device) async {
+    _foundedDevices.removeWhere((element) => element.address == device.address);
+    if (device.isBonded) {
+      await _bluetooth
+          .removeDeviceBondWithAddress(device.address)
+          .then((isUnPairSucessful) {
+        if (isUnPairSucessful!) {
+          emit(BluetoothResponse(
+              message: 'Device ${device.name} has been unpaired!'));
+        }
+      });
+    }
   }
 }
--- lib/screens/home/cubit/home_state.dart
+++ lib/screens/home/cubit/home_state.dart
@@ -2,13 +2,25 @@ part of 'home_cubit.dart';
 
 abstract class HomeState {}
 
-class Initial extends HomeState {}
-
-class Connected extends HomeState {}
-
-class Loading extends HomeState {
+class Initial extends HomeState {
   final String message;
-  Loading({required this.message});
+  Initial({required this.message});
+}
+
+class Paired extends HomeState {
+  final BluetoothDevice device;
+
+  Paired({required this.device});
+}
+
+class Pairing extends HomeState {
+  final String message;
+  Pairing({required this.message});
+}
+
+class Discovering extends HomeState {
+  final String message;
+  Discovering({required this.message});
 }
 
 class ShowDevices extends HomeState {
