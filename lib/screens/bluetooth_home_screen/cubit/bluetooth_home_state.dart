c14b40afce6969c32e72e17de1f026e7355403a6
mediocre9 <mirzafahadzia9@gmail.com>
2023-02-21 01:33:01 +0500

    refactor: code cleanup and refactoring

--- lib/screens/bluetooth_home_screen/cubit/bluetooth_home_state.dart
+++ lib/screens/bluetooth_home_screen/cubit/bluetooth_home_state.dart
@@ -1,46 +1,102 @@
+// ignore_for_file: slash_for_doc_comments
+
 part of 'bluetooth_home_cubit.dart';
 
 abstract class BluetoothHomeState {}
 
 class Initial extends BluetoothHomeState {
   final IconData icon;
-  final String message;
+  final String text;
 
   Initial({
-    required this.message,
+    required this.text,
     required this.icon,
   });
 }
 
-class DeviceHasbeenPaired extends BluetoothHomeState {
+class BluetoothDisabled extends BluetoothHomeState {
+  final String message;
+
+  BluetoothDisabled({required this.message});
+}
+
+/* 
+* Discovery States . . .
+*/
+class DiscoverNewDevices extends BluetoothHomeState {
+  final String text;
+  DiscoverNewDevices({required this.text});
+}
+
+// class HasFoundNewDevices extends BluetoothHomeState {
+//   final String message;
+//   final Color snackbarColor;
+//   HasFoundNewDevices({
+//     required this.snackbarColor,
+//     required this.message,
+//   });
+// }
+
+class HasNotFoundNewDevices extends BluetoothHomeState {
+  final String message;
+
+  HasNotFoundNewDevices({required this.message});
+}
+
+class ConnectToRemoteDevice extends BluetoothHomeState {
   final BluetoothDevice device;
 
-  DeviceHasbeenPaired({required this.device});
+  ConnectToRemoteDevice({required this.device});
 }
 
-class DeviceIsPairing extends BluetoothHomeState {
-  final String message;
-  DeviceIsPairing({required this.message});
-}
+class ShowDiscoveredDevices extends BluetoothHomeState {
+  final List<BluetoothDevice> discoveredDevices;
+  final int totalDiscoveredDevices;
 
-class DiscoveringDevices extends BluetoothHomeState {
-  final String message;
-  DiscoveringDevices({required this.message});
-}
-
-class FoundedBluetoothDevices extends BluetoothHomeState {
-  final List<BluetoothDevice> devices;
-
-  final int totalDevices;
-
-  FoundedBluetoothDevices({
-    required this.totalDevices,
-    required this.devices,
+  ShowDiscoveredDevices({
+    required this.totalDiscoveredDevices,
+    required this.discoveredDevices,
   });
 }
 
-class BluetoothOnGoingResponse extends BluetoothHomeState {
-  final String message;
+/** 
+ * Pairing states 
+ */
+class PairDevice extends BluetoothHomeState {
+  final String text;
+  PairDevice({required this.text});
+}
 
-  BluetoothOnGoingResponse({required this.message});
+class PairUnsuccessful extends BluetoothHomeState {
+  final String message;
+  final Color snackbarColor;
+  PairUnsuccessful({
+    required this.snackbarColor,
+    required this.message,
+  });
+}
+
+class PairSuccessful extends BluetoothHomeState {
+  final String message;
+  final Color snackbarColor;
+
+  PairSuccessful({
+    required this.message,
+    required this.snackbarColor,
+  });
+}
+
+class GetPairedDevices extends BluetoothHomeState {
+  final String text;
+  GetPairedDevices({required this.text});
+}
+
+class ShowPairedDevices extends BluetoothHomeState {
+  final List<BluetoothDevice> pairedDevices;
+  final int totalPairedDevices;
+
+  ShowPairedDevices({
+    required this.totalPairedDevices,
+    required this.pairedDevices,
+  });
 }
