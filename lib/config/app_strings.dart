54423a304841546ad3a19a9fbb94fe28e0c34cbc
mediocre9 <mirzafahadzia9@gmail.com>
2023-03-21 21:19:29 +0500

    config: update and add new values

--- lib/config/app_strings.dart
+++ lib/config/app_strings.dart
@@ -5,7 +5,7 @@ class AppString {
   AppString._();
 
   static const String APP_NAME = 'Smart Link';
-  static const String APP_VERSION = 'v0.1a';
+  static const String APP_VERSION = 'v0.3.0';
   static const String APP_DESCRIPTION = 'IoT Remote Control';
   static const String INITIAL_HOME_SCREEN = 'Scan for bluetooth devices';
   static const String DISCOVERING_MSG =
@@ -16,4 +16,13 @@ class AppString {
       'No nearby device(s) available. Try Again!';
   static const String COPYRIGHT =
       '(c) Copyright 2023 CUSIT IT & Robotics Engineering Society. All rights reserved.';
+
+  static const String TEMPORARY_LOCKED_OUT_ERROR =
+      "Locked out due to too many attempts!";
+  static const String PERMANENT_LOCKED_OUT_ERROR =
+      "Locked out permanently due to too many attempts!";
+  static const String BIOMETRIC_NOT_ENROLLED_ERROR =
+      "Please register your fingerprint from your device settings!";
+  static const String HARDWARE_SUPPORT_NOT_AVAILABLE =
+      "Your device does not have fingerprint support!";
 }
