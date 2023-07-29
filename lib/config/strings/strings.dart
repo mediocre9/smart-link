4f2dda370f1bcb930ea28bb360b16c408466f5e4
mediocre9 <mirzafahadzia9@gmail.com>
2023-07-29 05:47:11 -0700

    refactor: change constant variables naming convention from hungarian to camelCase

---
+++ lib/config/strings/strings.dart
@@ -0,0 +1,31 @@
+class Strings {
+  Strings._();
+
+  static const String appName = 'Smart Link';
+  static const String appVersion = 'v0.5.2';
+  static const String appDescription = 'IoT Remote Control';
+  static const String appLogo = 'assets/images/logo.png';
+  static const String bluetoothOnHomeDescription = 'Scan for bluetooth devices';
+  static const String bluetoothDiscoveryDescription =
+      'This may take a few moments to discover nearby devices. Please be patient!';
+  static const String noInternet = 'No Internet Connection!';
+  static const String bluetoothOff = 'Bluetooth service is off.';
+  static const String devicesNotInRange =
+      'No nearby device(s) available. Try Again!';
+  static const String copyright =
+      '(c) Copyright 2023 CUSIT IT & Robotics Engineering Society. All rights reserved.';
+
+  static const String biometricLock = "Locked out due to too many attempts!";
+  static const String biometricPermanent =
+      "Locked out permanently due to too many attempts!";
+  static const String biometricEnrollment =
+      "Please register your fingerprint from your device settings!";
+  static const String biometricNotSupported =
+      "Your device does not have fingerprint support!";
+  static const String microControllerIp = "192.168.4.1";
+
+  static const String userBlocked = 'Your account has been blocked.';
+  static const String googleLogoPath = 'assets/images/google_logo.png';
+  static const String radarAnimationPath = 'assets/animations/radar.json';
+  static const String signinButtonText = 'Sign in with Google';
+}
