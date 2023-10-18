cabd32d3069b2d6cb41436400a71c83583a8d83e
mediocre9 <mirzafahadzia9@gmail.com>
2023-10-18 19:51:23 +0500

    refactor: create and add string constants

--- lib/config/strings/strings.dart
+++ lib/config/strings/strings.dart
@@ -6,22 +6,18 @@ class Strings {
   static const String appDescription = 'IoT Remote Control';
   static const String appLogo = 'assets/images/logo.png';
   static const String bluetoothOnHomeDescription = 'Scan for bluetooth devices';
-  static const String bluetoothDiscoveryDescription =
-      'This may take a few moments to discover nearby devices. Please be patient!';
+  static const String bluetoothDiscoveryDescription = 'This may take a few moments to discover nearby devices. Please be patient!';
+  static const String gettingPairedDevices = 'Getting paired devices . . .';
+  static const String pairUnsuccessful = "";
   static const String noInternet = 'No Internet Connection!';
   static const String bluetoothOff = 'Bluetooth service is off.';
-  static const String devicesNotInRange =
-      'No nearby device(s) available. Try Again!';
-  static const String copyright =
-      '(c) Copyright 2023 CUSIT IT & Robotics Engineering Society. All rights reserved.';
+  static const String devicesNotInRange = 'No nearby device(s) available. Try Again!';
+  static const String copyright = '(c) Copyright 2023 CUSIT IT & Robotics Engineering Society. All rights reserved.';
 
   static const String biometricLock = "Locked out due to too many attempts!";
-  static const String biometricPermanent =
-      "Locked out permanently due to too many attempts!";
-  static const String biometricEnrollment =
-      "Please register your fingerprint from your device settings!";
-  static const String biometricNotSupported =
-      "Your device does not have fingerprint support!";
+  static const String biometricPermanent = "Locked out permanently due to too many attempts!";
+  static const String biometricEnrollment = "Please register your fingerprint from your device settings!";
+  static const String biometricNotSupported = "Your device does not have fingerprint support!";
   static const String microControllerIp = "192.168.4.1";
 
   static const String userBlocked = 'Your account has been blocked.';
