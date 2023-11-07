2095f13e773864236ff97324b4fedd817670a28b
mediocre9 <mirzafahadzia9@gmail.com>
2023-11-07 19:59:11 +0500

    (refactor/feat)r: change `establishConnection` method to more meaningful name with `connectToESP8266` method and add wifi settings panel

--- lib/screens/wifi_home_screen/cubit/wifi_home_cubit.dart
+++ lib/screens/wifi_home_screen/cubit/wifi_home_cubit.dart
@@ -1,15 +1,16 @@
+import 'package:app_settings/app_settings.dart';
 import 'package:bloc/bloc.dart';
 import 'package:http/http.dart';
 
-import '../../../config/strings/strings.dart';
+import '../../../config/strings/app_strings.dart';
 part 'wifi_home_state.dart';
 
 class WifiHomeCubit extends Cubit<WifiHomeState> {
   WifiHomeCubit() : super(Initial());
   String? baseUrl;
 
-  Future<void> establishConnection() async {
-    baseUrl = "http://${Strings.microControllerIp}/";
+  Future<void> connectToESP8266() async {
+    baseUrl = "http://${AppStrings.deviceServerIP}/";
     emit(Connecting());
     try {
       Response response = await get(Uri.parse(baseUrl!));
@@ -19,6 +20,8 @@ class WifiHomeCubit extends Cubit<WifiHomeState> {
     } catch (e) {
       emit(NotConnected("Connection failed!"));
       emit(Initial());
+      await Future.delayed(const Duration(seconds: 2));
+      await AppSettings.openAppSettingsPanel(AppSettingsPanelType.wifi);
     }
   }
 }
