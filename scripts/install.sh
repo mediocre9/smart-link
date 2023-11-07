393ef937fd5ccc2fcb7a8a6c0979fd9497dfaa64
mediocre9 <mirzafahadzia9@gmail.com>
2023-11-07 20:12:30 +0500

    refactor: add `adb` shell cmd to launch app after installation

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,10 @@
 # change app architecture from here
 # according to your needs . . .
 app_architecture="app-armeabi-v7a-release.apk"
+package="com.mediocre.smartlink"
+
 adb install -r "../build/app/outputs/flutter-apk/$app_architecture"
+adb shell am start -n $package/.MainActivity
+
+echo Success
 read
