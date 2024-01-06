ae45f0043f66fac2ba058378a7163694a3c7c2f6
mediocre9 <mirzafahadzia9@gmail.com>
2024-01-07 00:02:02 +0500

    refactor: improve code and it's flow and use enums for state handling instead of using State classes

--- lib/screens/wifi_remote_screen/cubit/wifi_remote_cubit.dart
+++ lib/screens/wifi_remote_screen/cubit/wifi_remote_cubit.dart
@@ -3,35 +3,40 @@ import 'dart:io';
 
 import 'package:bloc/bloc.dart';
 import 'package:http/http.dart' as http;
-part 'wifi_remote_state.dart';
+
+enum WifiRemoteState {
+  loading,
+  onSignal,
+  offSignal,
+}
 
 class WifiRemoteCubit extends Cubit<WifiRemoteState> {
-  WifiRemoteCubit() : super(OffSignal());
+  WifiRemoteCubit() : super(WifiRemoteState.offSignal);
 
   Future<void> sendOnMessage(String baseUrl) async {
-    emit(Loading());
+    emit(WifiRemoteState.loading);
 
     try {
       await http.get(
-        Uri.parse('${baseUrl}on_signal'),
+        Uri.parse('$baseUrl/on_signal'),
         headers: {"Accept": "plain/text"},
       );
+      emit(WifiRemoteState.onSignal);
     } on HttpException catch (e) {
       log(e.message);
     }
-    emit(OnSignal());
   }
 
   Future<void> sendOffMessage(String baseUrl) async {
-    emit(Loading());
+    emit(WifiRemoteState.loading);
     try {
       await http.get(
-        Uri.parse('${baseUrl}off_signal'),
+        Uri.parse('$baseUrl/off_signal'),
         headers: {"Accept": "plain/text"},
       );
+      emit(WifiRemoteState.offSignal);
     } on HttpException catch (e) {
       log(e.message);
     }
-    emit(OffSignal());
   }
 }
