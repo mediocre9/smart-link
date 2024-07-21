32fcefe876e03d18e20dd002a6bfb6734ceec2cd
mediocre9 <mirzafahadzia9@gmail.com>
2024-07-21 18:20:57 +0500

    refactor: remove dead code files

--- lib/screens/wifi_remote_screen/cubit/wifi_remote_state.dart
+++
@@ -1,9 +0,0 @@
-part of 'wifi_remote_cubit.dart';
-
-sealed class WifiRemoteState {}
-
-class Loading extends WifiRemoteState {}
-
-class OnSignal extends WifiRemoteState {}
-
-class OffSignal extends WifiRemoteState {}
