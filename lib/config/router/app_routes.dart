2852366e55208be0911726b7b7e1864516390af3
mediocre9 <mirzafahadzia9@gmail.com>
2024-07-22 20:16:04 +0500

    refactor: remove dead code and set bluetooth home as initial route

--- lib/config/router/app_routes.dart
+++ lib/config/router/app_routes.dart
@@ -1,17 +1,13 @@
 class AppRoutes {
   AppRoutes._();
 
-  static const String auth = '/';
+  static const String bluetoothHome = 'bluetoothHome';
 
-  static const String bluetoothHome = '/bluetoothHome';
+  static const String auth = '/auth';
 
   static const String bluetoothRemote = '/bluetoothRemote';
 
   static const String wifiHome = '/wifiHome';
-
-  static const String wifiRemote = '/wifiRemote';
-
-  static const String biometric = '/biometric';
 
   static const String feedback = '/feedback';
 }
